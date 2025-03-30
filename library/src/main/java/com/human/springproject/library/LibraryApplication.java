0ddc8d525de1e10d26c98c5347d8725837ea5bf3
hyunwoo <hyunwoo_9800@naver.com>
2025-03-30 18:51:19 +0900

    [add] 도서관 프로젝트 서버 연결 테스트 2025/03/30 @3.5h

--- library/src/main/java/com/human/springproject/library/LibraryApplication.java
+++ library/src/main/java/com/human/springproject/library/LibraryApplication.java
@@ -1,4 +1,4 @@
-package com.human.springproject.library.controller;
+package com.human.springproject.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
